1d692283196a6c3c781d7277d2c7e64ff3062f6d
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-03-13 20:03:19 +0100

    Add logging

--- src/foo/foo.controller.ts
+++ src/foo/foo.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 
 @Controller('api/foo')
 export class FooController {
+  private readonly logger;
+
+  constructor() {
+    this.logger = new Logger(FooController.name);
+  }
+
   @Get()
   getMy(): string {
+    console.log('GET to /api/foo');
+    this.logger.error('GET to /api/foo');
+    this.logger.warn('GET to /api/foo');
+    this.logger.log('GET to /api/foo');
+    this.logger.debug('GET to /api/foo');
+    this.logger.verbose('GET to /api/foo');
     return "Foo!";// this.appService.getHello();
   }
 }
