bb4e8459cfdc4c7c16e3301ab67d19585cc75e4f
M.D. van Es <es@mdworld.nl>
2020-03-15 16:04:29 +0100

    Serve login page

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get, Logger, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from '../auth/local-auth.guard';
 import { AuthService } from '../auth/auth.service';
+import * as path from 'path';
+import * as fs from 'fs';
+// const fs = require('fs');
+// import * as foo from './index.html';
 
 @Controller('auth/login')
 export class LoginController {
@@ -14,8 +18,22 @@ export class LoginController {
   @Get()
   getFoo(): string {
     this.logger.verbose('GET to /auth/login');
-    // TODO should serve the login page
-    return "The Login Page";
+
+    // TODO load without absolute path, should work in webpack
+    const html = fs.readFileSync('/home/martin/ZNoBackup/homeremote-nestjs-server/src/login/index.html', 'utf-8');
+    // const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+    return html;
+//     return `
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <title>Login</title>
+// </head>
+// <body>
+// Login
+// </body>
+// </html>`;
   }
 
   @UseGuards(LocalAuthGuard)
