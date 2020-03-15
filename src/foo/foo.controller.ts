90f900886a91e08ce13b369ef3138a35e213672e
M.D. van Es <es@mdworld.nl>
2020-03-15 15:35:17 +0100

    Add JWT authentication

--- src/foo/foo.controller.ts
+++ src/foo/foo.controller.ts
@@ -1,30 +1,12 @@
-import { Controller, Get, Logger, Post, Request, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from '../auth/local-auth.guard';
-import { AuthService } from '../auth/auth.service';
+import { Controller, Get, Logger, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('api/foo')
 export class FooController {
   private readonly logger;
 
-  constructor(private readonly authService: AuthService) {
+  constructor() {
     this.logger = new Logger(FooController.name);
-  }
-
-  // TODO extract to /login controller
-  // this is ending up at /api/foo/auth/login
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    console.log(`login: ${req.user}`);
-    return this.authService.login(req.user);
-  }
-
-  // TODO extract to /api/profile and use this to show the "logged in as user"
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
   }
 
   @UseGuards(JwtAuthGuard)
@@ -36,6 +18,6 @@ export class FooController {
     this.logger.log('GET to /api/foo');
     this.logger.debug('GET to /api/foo');
     this.logger.verbose('GET to /api/foo');
-    return "Foo!";// this.appService.getHello();
+    return "Foo!";
   }
 }
