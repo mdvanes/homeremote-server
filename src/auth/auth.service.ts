53f043e5dff3f9f0a783c28ebfddc98e5282943c
M.D. van Es <es@mdworld.nl>
2020-03-16 20:09:26 +0100

    Clean up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,17 +3,6 @@ import { JwtService } from '@nestjs/jwt';
 import bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
 
-/*
-TODO
-
-Use bcrypt
-
-WARNING
-Of course in a real application, you wouldn't store a password in plain text. You'd instead use a library like bcrypt, with a salted one-way hash algorithm.
-With that approach, you'd only store hashed passwords, and then compare the stored password to a hashed version of the incoming password, thus never storing
-or exposing user passwords in plain text. To keep our sample app simple, we violate that absolute mandate and use plain text. Don't do this in your real app!
- */
-
 @Injectable()
 export class AuthService {
   private readonly logger;
