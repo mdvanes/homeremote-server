44346b2cb9ac2fe0505bd844b82f1834cee0e803
M.D. van Es <es@mdworld.nl>
2020-08-30 18:44:34 +0200

    Fix retrieve user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,6 +12,6 @@ export class UsersService {
   }
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+    return this.users.find(({ name }) => name === username);
   }
 }
