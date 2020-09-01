d9a9a0ec3a8c850efd75637f4c002594ef9f84c7
M.D. van Es <es@mdworld.nl>
2020-09-01 09:02:24 +0200

    Migrate Switches route

---
+++ src/switches/switches.controller.ts
@@ -0,0 +1,154 @@
+import { Controller, Logger, Get } from '@nestjs/common';
+import got from "got";
+
+// TODO ConfigService instead of settings.json https://docs.nestjs.com/techniques/configuration#using-the-configservice
+// "domoticzuri": "http://192.168.0.8:8080",
+const settings = {
+    "domoticzuri": "http://192.168.0.8:8080",
+}
+
+enum DomoticzType {
+    Group = "Group",
+    LightSwitch = "Light/Switch",
+    Dimmer = "Dimmer",
+    Selector = 'Selector'
+}
+
+type DomoticzStatus = string;
+
+interface DomoticzSwitch {
+    idx: string;
+    Type: DomoticzType;
+    Name: string;
+    Status: DomoticzStatus;
+    SwitchType: DomoticzType;
+    Level: number;
+    Protected: boolean;
+}
+
+interface HomeRemoteSwitch {
+    idx: string;
+    type: DomoticzType;
+    status: DomoticzStatus;
+    name: string;
+    dimLevel: number | null;
+    readOnly: boolean;
+    children?: HomeRemoteSwitch[] | false;
+}
+
+interface SwitchesResponse {
+    status: "received" | "error";
+    switches?: HomeRemoteSwitch[]
+}
+
+type GetIsOnDimmer = (SwitchType: DomoticzType, Status: string) => boolean;
+const getIsOnDimmer: GetIsOnDimmer = (SwitchType, Status) =>
+    SwitchType === 'Dimmer' && Status === 'On';
+
+type GetDimLevel = (isOnDimmer: boolean, isSelector: boolean, Level: number) => number | null;
+const getDimLevel: GetDimLevel = (isOnDimmer, isSelector, Level) =>
+    isOnDimmer || isSelector ? Level : null;
+
+// For switches included in a scene
+type MapIncludedSwitch = (SceneStatus: DomoticzStatus) => ({ DevID, Type, Name }: { DevID: string, Type: DomoticzType, Name: string }) => HomeRemoteSwitch;
+const mapIncludedSwitch: MapIncludedSwitch = SceneStatus => ({ DevID, Type, Name }) => ({
+    idx: DevID,
+    type: Type,
+    name: Name,
+    status: SceneStatus,
+    dimLevel: null, // NYI, to implement this get each switch detail by DevID on /json.htm?type=command&param=getlightswitches
+    readOnly: false // NYI, to implement this get each switch detail by DevID on /json.htm?type=command&param=getlightswitches
+});
+
+type GetChildren = (SceneIdx: string, SceneType: DomoticzType, SceneStatus: DomoticzStatus) => Promise<HomeRemoteSwitch[] | false>;
+const getChildren: GetChildren = async (SceneIdx, SceneType, SceneStatus) => {
+    if (SceneType === 'Group') {
+        const targetUri = `${settings.domoticzuri}/json.htm?type=command&param=getscenedevices&idx=${SceneIdx}&isscene=true`;
+        const remoteResponse = await got(targetUri);
+        const remoteResponseJson = JSON.parse(remoteResponse.body);
+        if (remoteResponseJson.status === 'OK') {
+            return remoteResponseJson.result.map(
+                mapIncludedSwitch(SceneStatus)
+            );
+        }
+        return false;
+    }
+    return false;
+};
+
+const mapSwitch = async ({
+    idx,
+    Type,
+    Name,
+    Status,
+    SwitchType,
+    Level,
+    Protected
+}: DomoticzSwitch): Promise<HomeRemoteSwitch> => {
+    const isSelector = SwitchType === 'Selector';
+    const isOnDimmer = getIsOnDimmer(SwitchType, Status);
+    const children = await getChildren(idx, Type, Status);
+    const switchResult: HomeRemoteSwitch = {
+        idx,
+        type: isSelector ? SwitchType : Type,
+        name: Name,
+        status: Status,
+        dimLevel: getDimLevel(isOnDimmer, isSelector, Level),
+        readOnly: Protected,
+        children
+    };
+    return switchResult;
+};
+
+@Controller('api/switches')
+export class SwitchesController {
+    private readonly logger: Logger;
+
+    constructor() {
+        this.logger = new Logger(SwitchesController.name);
+    }
+
+    // @UseGuards(JwtAuthGuard)
+    @Get()
+    async getSwitches(): Promise<SwitchesResponse> {
+        //   console.log('GET to /api/switches');
+        //   this.logger.error('GET to /api/switches');
+        //   this.logger.warn('GET to /api/switches');
+        //   this.logger.log('GET to /api/switches');
+        //   this.logger.debug('GET to /api/switches');
+        // this.logger.verbose('GET to /api/switches');
+        // return [];
+
+        this.logger.verbose(
+            `GET to /api/switches domoticzuri: ${
+            settings.domoticzuri
+            }]`
+        );
+        if (settings.domoticzuri && settings.domoticzuri.length > 0) {
+            const targetUri = `${settings.domoticzuri}/json.htm?type=devices&used=true&filter=all&favorite=1`;
+            try {
+                const remoteResponse = await got(targetUri);
+                const remoteResponseJson = JSON.parse(remoteResponse.body);
+                if (remoteResponseJson.status === 'OK') {
+                    const switches = await Promise.all(
+                        (remoteResponseJson.result as DomoticzSwitch[]).map(mapSwitch)
+                    );
+                    // this.logger.verbose(`SWITCHES ${switches} x= ${typeof switches[0]}, z=${JSON.stringify(switches[0])} json=${JSON.stringify(remoteResponseJson)}`);
+                    return {
+                        status: 'received',
+                        switches
+                    };
+                } else {
+                    // noinspection ExceptionCaughtLocallyJS
+                    throw new Error('remoteResponse failed');
+                }
+            } catch (err) {
+                this.logger.error(`Can't parse response ${err}`);
+                return { status: 'error' };
+            }
+        } else {
+            this.logger.error('domoticzuri not configured');
+            return { status: 'error' };
+        }
+    }
+}
