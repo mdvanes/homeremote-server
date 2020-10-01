90ac21d55cabc5fb5741f991968dc235e18dd22a
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-10-01 19:58:52 +0200

    Add unit tests

--- src/switches/switches.controller.spec.ts
+++ src/switches/switches.controller.spec.ts
@@ -1,5 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { SwitchesController } from './switches.controller';
+import * as Got from 'got';
+import {
+  DomoticzSwitch,
+  DomoticzType,
+  SwitchesController,
+} from './switches.controller';
+
+jest.mock('got');
+const gotSpy = jest.spyOn(Got, 'default');
 
 describe('Switches Controller', () => {
   let controller: SwitchesController;
@@ -12,7 +20,57 @@ describe('Switches Controller', () => {
     controller = module.get<SwitchesController>(SwitchesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('returns the switches states on /GET ', async () => {
+    const mockSwitch: DomoticzSwitch = {
+      idx: '1',
+      Type: DomoticzType.LightSwitch,
+      Name: 'My Switch',
+      Status: 'on',
+      SwitchType: DomoticzType.LightSwitch,
+      Level: 0,
+      Protected: false,
+    };
+    gotSpy.mockResolvedValue({
+      body: JSON.stringify({ status: 'OK', result: [mockSwitch] }),
+    });
+    expect(await controller.getSwitches()).toEqual({
+      status: 'received',
+      switches: [
+        {
+          idx: '1',
+          type: 'Light/Switch',
+          name: 'My Switch',
+          status: 'on',
+          dimLevel: null,
+          readOnly: false,
+          children: false,
+        },
+      ],
+    });
+  });
+
+  it('returns error status on failed /GET ', async () => {
+    gotSpy.mockRejectedValue('Mock Server Down');
+    expect(await controller.getSwitches()).toEqual({
+      status: 'error',
+    });
+  });
+
+  it('returns error status on non-OK /GET ', async () => {
+    gotSpy.mockResolvedValue({
+      body: JSON.stringify({ status: 'error' }),
+    });
+    expect(await controller.getSwitches()).toEqual({
+      status: 'error',
+    });
+  });
+
+  it('returns error status on unparsable /GET ', async () => {
+    gotSpy.mockResolvedValue({
+      body: 'not json',
+    });
+    expect(await controller.getSwitches()).toEqual({
+      status: 'error',
+    });
   });
 });
