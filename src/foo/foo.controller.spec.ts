cb3bc8c89d62091ec8079d3d5b3d8f0f189a9f5d
M.D. van Es <es@mdworld.nl>
2020-10-03 10:54:49 +0200

    Use ts-jest util

--- src/foo/foo.controller.spec.ts
+++ src/foo/foo.controller.spec.ts
@@ -1,11 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { FooController } from './foo.controller';
+import { mocked } from 'ts-jest/utils'
 import { Logger } from '@nestjs/common';
+import { FooController } from './foo.controller';
 
 // Note: jest.mock fails with "@nestjs/common", but "@nestjs/common/services" and deeper is fine ¯\_(ツ)_/¯
 jest.mock('@nestjs/common/services');
 
-const MockLogger = Logger as unknown as jest.Mock<Logger>;
+const MockLogger = mocked(Logger, true); // not to have: Logger as unknown as jest.Mock<Logger>;
 
 describe('Foo Controller', () => {
   let controller: FooController;
@@ -25,10 +26,10 @@ describe('Foo Controller', () => {
     const result = await controller.getFoo();
     expect(result).toBe("Foo!");
     const fooLoggerInstance = MockLogger.mock.instances[2];
-    expect((fooLoggerInstance.error as unknown as jest.Mock).mock.calls[0][0]).toEqual("Error: GET to /api/foo");
-    expect((fooLoggerInstance.warn as unknown as jest.Mock).mock.calls[0][0]).toEqual("Warning: GET to /api/foo");
-    expect((fooLoggerInstance.log as unknown as jest.Mock).mock.calls[0][0]).toEqual("Log: GET to /api/foo");
-    expect((fooLoggerInstance.debug as unknown as jest.Mock).mock.calls[0][0]).toEqual("Debug: GET to /api/foo");
-    expect((fooLoggerInstance.verbose as unknown as jest.Mock).mock.calls[0][0]).toEqual("Verbose: GET to /api/foo");
+    expect((mocked(fooLoggerInstance.error)).mock.calls[0][0]).toEqual("Error: GET to /api/foo");
+    expect((mocked(fooLoggerInstance.warn)).mock.calls[0][0]).toEqual("Warning: GET to /api/foo");
+    expect((mocked(fooLoggerInstance.log)).mock.calls[0][0]).toEqual("Log: GET to /api/foo");
+    expect((mocked(fooLoggerInstance.debug)).mock.calls[0][0]).toEqual("Debug: GET to /api/foo");
+    expect((mocked(fooLoggerInstance.verbose)).mock.calls[0][0]).toEqual("Verbose: GET to /api/foo");
   });
 });
