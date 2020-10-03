3b69b4c649ebeab6b54db235c76271fc1920d7b5
M.D. van Es <es@mdworld.nl>
2020-10-03 10:25:46 +0200

    Unit test foo controller

--- src/foo/foo.controller.spec.ts
+++ src/foo/foo.controller.spec.ts
@@ -1,10 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FooController } from './foo.controller';
 
+import { Logger } from '@nestjs/common/services/logger.service';
+jest.mock('@nestjs/common/services/logger.service');
+
+// const c: any = jest.genMockFromModule("@nestjs/common")
+// const c: any = jest.genMockFromModule("@nestjs/common/services/logger.service")
+
+// jest.spyOn(Common)
+
+// import { Controller, Get, Logger, UseGuards } from '@nestjs/common';
+// import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+// @Controller('api/foo')
+// export class FooController {
+//   private readonly logger: Logger;
+
 describe('Foo Controller', () => {
   let controller: FooController;
 
   beforeEach(async () => {
+    // @ts-ignore
+    Logger.mockReset();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FooController],
     }).compile();
@@ -12,7 +30,23 @@ describe('Foo Controller', () => {
     controller = module.get<FooController>(FooController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('logs a lot of stuff', async () => {
+    // @ts-ignore
+    // Logger.mockReset();
+    expect(Logger).toHaveBeenCalledWith("FooController");
+    const result = await controller.getFoo();
+    expect(result).toBe("Foo!");
+    // expect(Logger).not.toHaveBeenCalled();
+    // instances[2] is the FooController instance
+    // @ts-ignore
+    const fooLoggerInstance = Logger.mock.instances[2];
+    expect(fooLoggerInstance.error.mock.calls[0][0]).toEqual("Error: GET to /api/foo");
+    expect(fooLoggerInstance.warn.mock.calls[0][0]).toEqual("Warning: GET to /api/foo");
+    // expect(c.Logger.constructor.name).toBe("a");
+    // const mockLoggerInstance = c.Logger.mock
+    //   .instances[0];
+    //   // https://jestjs.io/docs/en/jest-object#jestcreatemockfrommodulemodulename
+    // expect(mockLoggerInstance.error).toHaveBeenCalledWith(''); // TODO hmm...
+    // expect(result).toBe('Foo!');
   });
 });
