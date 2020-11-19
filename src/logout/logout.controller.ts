d251c76b58db791e1052b719485cb10b016e5713
M.D. van Es <es@mdworld.nl>
2020-11-19 19:08:26 +0100

    Add logout

--- src/logout/logout.controller.ts
+++ src/logout/logout.controller.ts
@@ -1,22 +1,28 @@
 import { Controller, Get, Logger, Req } from '@nestjs/common';
-import { AuthService } from '../auth/auth.service';
+import { Request } from 'express';
+import { User } from '../users/users.service';
+import { AuthService, EXPIRES_IN_S } from '../auth/auth.service';
 
 @Controller('auth/logout')
 export class LogoutController {
+  private readonly logger: Logger;
 
-    private readonly logger: Logger;
+  constructor(private readonly authService: AuthService) {
+    this.logger = new Logger(LogoutController.name);
+  }
 
-    constructor(private readonly authService: AuthService) {
-      this.logger = new Logger(LogoutController.name);
+  @Get()
+  async logout(@Req() req: Request): Promise<User> {
+    this.logger.verbose(`logout: ${JSON.stringify(req.user)}`);
+    if (req.res) {
+      req.res.setHeader(
+        'Set-Cookie',
+        `Authentication=; HttpOnly; Path=/; ${EXPIRES_IN_S}`, // Max-Age in seconds.
+      );
     }
-
-    @Get()
-    async logout(@Req() req: any) {
-      this.logger.verbose(`logout: ${JSON.stringify(req.user)}`);
-      if (req.res) {
-        req.res.setHeader('Set-Cookie', `Authentication=; HttpOnly; Path=/; Max-Age=10`);
-      }
-      return req.user;
-    }
-
+    return {
+      id: 0,
+      name: '',
+    };
+  }
 }
