01829a9cf90ed77c8246e6acf63b541da9ca259a
M.D. van Es <es@mdworld.nl>
2020-11-21 16:40:47 +0100

    Add status unit test

--- src/status/status.controller.spec.ts
+++ src/status/status.controller.spec.ts
@@ -1,20 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
 import { StatusController } from './status.controller';
 
 describe('Status Controller', () => {
   let controller: StatusController;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StatusController],
+      providers: [{ provide: ConfigService, useValue: { get: jest.fn() } }],
     }).compile();
 
+    configService = module.get<ConfigService>(ConfigService);
     controller = module.get<StatusController>(StatusController);
   });
 
   it('returns the application status on /GET', async () => {
-    expect(controller).toBeDefined();
+    jest.spyOn(configService, 'get').mockImplementation((envName) => {
+      if (envName === 'STATUS_CMD') {
+        return 'echo { \\"field1\\": \\"field1value\\" }';
+      }
+      return 'field1';
+    });
     const result = await controller.getStatus();
-    expect(result).toEqual({ status: expect.stringContaining('Tabcdafaf') });
+    expect(result).toEqual({ status: "field1value field1value" });
   });
 });
