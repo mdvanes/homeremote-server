02d116895a4f07d2d6f686de0572fbd52f9c68f6
M.D. van Es <es@mdworld.nl>
2020-11-21 16:33:22 +0100

    Implement status controller

--- src/status/status.controller.ts
+++ src/status/status.controller.ts
@@ -1,28 +1,53 @@
 import {
   Controller,
   Get,
-  // HttpException,
-  // HttpStatus,
+  HttpException,
+  HttpStatus,
   Logger,
   UseGuards,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { exec } from 'child_process';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+const execToJson = (statusCmd: string): Promise<Record<string, string>> =>
+  new Promise((resolve, reject) => {
+    exec(statusCmd, (error, stdout, stderr) => {
+      if (error) {
+        reject(`exec error ${error}`);
+      }
+      if (stderr) {
+        reject(`exec stderr ${stderr}`);
+      }
+      try {
+        const json = JSON.parse(stdout);
+        resolve(json);
+      } catch (err) {
+        reject(`exec parse ${err}`);
+      }
+    });
+  });
 
 @Controller('api/status')
 export class StatusController {
   private readonly logger: Logger;
 
-  constructor() {
+  constructor(private configService: ConfigService) {
     this.logger = new Logger(StatusController.name);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get()
   async getStatus(): Promise<{ status: string }> {
-    // TODO get command from "service configuration" and execute
-    return { status: 'Tabcdafaf ' + Date.now() };
-    // const error = new Error('fake error');
-    // this.logger.error(error);
-    // throw new HttpException(error, HttpStatus.NOT_IMPLEMENTED);
+    const statusCmd = this.configService.get<string>('STATUS_CMD') || '';
+    const statusField1 = this.configService.get<string>('STATUS_FIELD1') || '';
+    const statusField2 = this.configService.get<string>('STATUS_FIELD2') || '';
+    try {
+      const json = await execToJson(statusCmd);
+      return { status: `${json[statusField1]} ${json[statusField2]}` };  
+    } catch (err) {
+      this.logger.error(err);
+      throw new HttpException("error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
