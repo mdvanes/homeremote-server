80dd8473d2f45d58fc5896b415fff558307360f4
M.D. van Es <es@mdworld.nl>
2020-11-29 11:19:24 +0100

    Set meta data

---
+++ src/urltomusic/id3writer.d.ts
@@ -0,0 +1,29 @@
+declare module 'id3-writer' {
+  class FileType {}
+
+  class MetaType {}
+
+  class WriterType {
+    setFile(
+      file: FileType,
+    ): {
+      write: (meta: any, callback: any) => any;
+    };
+  }
+
+  const Id3Writer: {
+    File: new (path: string) => FileType;
+    Meta: new ({
+      artist,
+      title,
+      album,
+    }: {
+      artist: string;
+      title: string;
+      album: string;
+    }) => MetaType;
+    Writer: new () => WriterType;
+  };
+
+  export = Id3Writer;
+}
