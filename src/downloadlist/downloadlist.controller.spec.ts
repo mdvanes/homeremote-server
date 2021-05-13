04eed8eeeff7032df45c97155d055e049d331af9
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-05-13 09:47:34 +0200

    Add unit test

--- src/downloadlist/downloadlist.controller.spec.ts
+++ src/downloadlist/downloadlist.controller.spec.ts
@@ -1,18 +1,166 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { DownloadlistController } from './downloadlist.controller';
+import { ConfigService } from "@nestjs/config";
+import { Test, TestingModule } from "@nestjs/testing";
+import { Transmission } from "@ctrl/transmission";
+import { mocked } from "ts-jest/utils";
+import { NormalizedTorrent, TorrentState } from "@ctrl/shared-torrent";
+import { DownloadlistController } from "./downloadlist.controller";
 
-describe('Downloadlist Controller', () => {
-  let controller: DownloadlistController;
+const mockResponse: Partial<NormalizedTorrent>[] = [
+    {
+        id: 1,
+        name: "some_name",
+        state: TorrentState.paused,
+        totalSize: 100000,
+        progress: 0.5,
+        downloadSpeed: 100,
+        uploadSpeed: 100,
+        eta: 1234567,
+    },
+    {
+        id: 2,
+        name: "some_other_name",
+        state: TorrentState.downloading,
+        totalSize: 100000,
+        progress: 0.1,
+        downloadSpeed: 100,
+        uploadSpeed: 100,
+        eta: 0,
+    },
+];
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [DownloadlistController],
-    }).compile();
+const mockResumeTorrent = jest.fn().mockResolvedValue({ result: "mock-ok" });
+const mockPauseTorrent = jest.fn().mockResolvedValue({ result: "mock-ok" });
+const mockGetAllData = jest.fn().mockResolvedValue({ torrents: mockResponse });
 
-    controller = module.get<DownloadlistController>(DownloadlistController);
-  });
+jest.mock("@ctrl/transmission", () => {
+    return {
+        Transmission: jest.fn().mockImplementation(() => {
+            return {
+                resumeTorrent: mockResumeTorrent,
+                pauseTorrent: mockPauseTorrent,
+                getAllData: mockGetAllData,
+            };
+        }),
+    };
+});
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+const MockTransmission = mocked(Transmission, true); // not to have: Transmission as unknown as jest.Mock<Logger>;
+
+describe("Downloadlist Controller", () => {
+    let controller: DownloadlistController;
+    let configService: ConfigService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [DownloadlistController],
+            providers: [
+                { provide: ConfigService, useValue: { get: jest.fn() } },
+            ],
+        }).compile();
+
+        configService = module.get<ConfigService>(ConfigService);
+        controller = module.get<DownloadlistController>(DownloadlistController);
+
+        jest.spyOn(configService, "get").mockReturnValueOnce("http://some_url");
+        jest.spyOn(configService, "get").mockReturnValueOnce("some_username");
+        jest.spyOn(configService, "get").mockReturnValueOnce("some_password");
+    });
+
+    describe("getDownloadList GET", () => {
+        it("returns a mapped list of current downloads", async () => {
+            const result = await controller.getDownloadList();
+            expect(MockTransmission).toHaveBeenCalledWith({
+                baseUrl: "http://some_url",
+                password: "some_password",
+                username: "some_username",
+            });
+            expect(result).toEqual({
+                status: "received",
+                downloads: [
+                    {
+                        downloadSpeed: "100 B",
+                        eta: "14d",
+                        id: 1,
+                        name: "some_name",
+                        percentage: 50,
+                        simpleState: "paused",
+                        size: "100 kB",
+                        state: "paused",
+                        uploadSpeed: "100 B",
+                    },
+                    {
+                        downloadSpeed: "100 B",
+                        eta: "",
+                        id: 2,
+                        name: "some_other_name",
+                        percentage: 10,
+                        simpleState: "downloading",
+                        size: "100 kB",
+                        state: "downloading",
+                        uploadSpeed: "100 B",
+                    },
+                ],
+            });
+        });
+
+        it("can return an error on failure", async () => {
+            mockGetAllData.mockRejectedValue("mock getAllData failed");
+
+            const result = await controller.getDownloadList();
+            expect(MockTransmission).toHaveBeenCalledWith({
+                baseUrl: "http://some_url",
+                password: "some_password",
+                username: "some_username",
+            });
+            expect(result).toEqual({ status: "error" });
+        });
+    });
+
+    describe("pauseDownload GET", () => {
+        it("can pause a download", async () => {
+            const result = await controller.pauseDownload(14);
+            expect(MockTransmission).toHaveBeenCalledWith({
+                baseUrl: "http://some_url",
+                password: "some_password",
+                username: "some_username",
+            });
+            expect(result).toEqual({ status: "received", message: "mock-ok" });
+        });
+
+        it("can return an error on failure", async () => {
+            mockPauseTorrent.mockRejectedValue("mock pause failed");
+
+            const result = await controller.pauseDownload(15);
+            expect(MockTransmission).toHaveBeenCalledWith({
+                baseUrl: "http://some_url",
+                password: "some_password",
+                username: "some_username",
+            });
+            expect(result).toEqual({ status: "error" });
+        });
+    });
+
+    describe("resumeDownload GET", () => {
+        it("can resume a download", async () => {
+            const result = await controller.resumeDownload(16);
+            expect(MockTransmission).toHaveBeenCalledWith({
+                baseUrl: "http://some_url",
+                password: "some_password",
+                username: "some_username",
+            });
+            expect(result).toEqual({ status: "received", message: "mock-ok" });
+        });
+
+        it("can return an error on failure", async () => {
+            mockResumeTorrent.mockRejectedValue("mock resume failed");
+
+            const result = await controller.resumeDownload(17);
+            expect(MockTransmission).toHaveBeenCalledWith({
+                baseUrl: "http://some_url",
+                password: "some_password",
+                username: "some_username",
+            });
+            expect(result).toEqual({ status: "error" });
+        });
+    });
 });
