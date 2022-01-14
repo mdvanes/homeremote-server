381c034f46fe9dbf36de6860321f3c3fc7bbe306
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-01-14 15:40:10 +0100

    Add tests

--- src/datalora/datalora.controller.spec.ts
+++ src/datalora/datalora.controller.spec.ts
@@ -2,6 +2,29 @@ import { ConfigService } from "@nestjs/config";
 import { Test, TestingModule } from "@nestjs/testing";
 import { DataloraController } from "./datalora.controller";
 
+const mockCollectRows = jest.fn().mockResolvedValue([]);
+
+jest.mock("@influxdata/influxdb-client", () => {
+    return {
+        InfluxDB: jest.fn().mockImplementation(() => ({
+            getQueryApi: jest.fn().mockReturnValue({
+                collectRows: mockCollectRows,
+            }),
+        })),
+    };
+});
+
+const identity = <T>(x: T): T => x;
+const collectRowsCreator = (rows: any[]) => (query: string, mapperFn: any) => {
+    return rows.map((x) => mapperFn(x, { toObject: identity }));
+};
+
+const MOCK_ROWS = [
+    { _value: "[1,2]", _time: "123" },
+    { _value: 111, _time: "123" },
+];
+const MOCK_DATA = [{ loc: [1, 2], time: "123" }];
+
 describe("Datalora Controller", () => {
     let controller: DataloraController;
     let configService: ConfigService;
@@ -28,9 +51,58 @@ describe("Datalora Controller", () => {
                 return "some.url";
             }
         });
+
+        mockCollectRows.mockReset();
     });
 
-    it("should be defined", () => {
-        expect(controller).toBeDefined();
+    it("returns coords for /GET with ?type=24h", async () => {
+        mockCollectRows.mockImplementation(collectRowsCreator(MOCK_ROWS));
+
+        const response = await controller.getCoords({ type: "24h" });
+        expect(response).toEqual({ data: MOCK_DATA });
+        expect(mockCollectRows).toBeCalledWith(
+            expect.stringContaining("range(start: -24h)"),
+            expect.anything()
+        );
+        expect(mockCollectRows).toBeCalledTimes(1);
+    });
+
+    it("returns coords for /GET with ?type=24h and falls back to type=all if no results", async () => {
+        mockCollectRows.mockImplementation(collectRowsCreator(MOCK_ROWS));
+        mockCollectRows.mockImplementationOnce(collectRowsCreator([]));
+
+        const response = await controller.getCoords({ type: "24h" });
+        expect(response).toEqual({ data: MOCK_DATA });
+        expect(mockCollectRows).toBeCalledWith(
+            expect.stringContaining("range(start: -24h)"),
+            expect.anything()
+        );
+        expect(mockCollectRows).toBeCalledWith(
+            expect.stringContaining("range(start: 0)"),
+            expect.anything()
+        );
+        expect(mockCollectRows).toBeCalledTimes(2);
+    });
+
+    it("returns coords for /GET with ?type=all", async () => {
+        mockCollectRows.mockImplementation(collectRowsCreator(MOCK_ROWS));
+
+        const response = await controller.getCoords({ type: "all" });
+        expect(response).toEqual({ data: MOCK_DATA });
+        expect(mockCollectRows).toBeCalledWith(
+            expect.stringContaining("range(start: 0)"),
+            expect.anything()
+        );
+        expect(mockCollectRows).toBeCalledTimes(1);
+    });
+
+    it("throws error when InfluxDb fails", async () => {
+        mockCollectRows.mockImplementation(() => {
+            throw new Error("some error");
+        });
+
+        await expect(controller.getCoords({ type: "all" })).rejects.toThrow(
+            "failed to receive downstream data"
+        );
     });
 });
